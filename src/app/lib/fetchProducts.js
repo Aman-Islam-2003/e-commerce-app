83623eaf76b08092d390f1f5d6c5aecc08a11afb
Aman Islam <amanislam019@gmail.com>
2025-04-02 23:51:17 +0500

    Update fetchProducts.js

--- src/app/lib/fetchProducts.js
+++ src/app/lib/fetchProducts.js
@@ -1,25 +1,25 @@
-// export async function fetchProducts() {
-//     try {
-//       const response = await fetch('https://fakestoreapi.com/products');
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch products');
-//       }
-//       return response.json();
-//     } catch (error) {
-//       console.error('Error fetching products:', error);
-//       return [];
-//     }
-//   }
+export async function fetchProducts() {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) {
+        throw new Error('Failed to fetch products');
+      }
+      return response.json();
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return [];
+    }
+  }
   
-//   export async function fetchProductById(id) {
-//     try {
-//       const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch product');
-//       }
-//       return response.json();
-//     } catch (error) {
-//       console.error('Error fetching product:', error);
-//       return null;
-//     }
-//   }
+  export async function fetchProductById(id) {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch product');
+      }
+      return response.json();
+    } catch (error) {
+      console.error('Error fetching product:', error);
+      return null;
+    }
+  }
